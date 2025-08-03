1a41eb5f6461d31eeff5fdd0ef02ccb202d814bb
Nicolas Marin <nicolasmarin1312@gmail.com>
2025-08-02 23:39:57 -0300

    Change on the them and some colors added

--- app/src/main/java/com/qrcraft/ui/theme/Color.kt
+++ app/src/main/java/com/qrcraft/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.qrcraft.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Yellow = Color(0xFFEBFF69)
+val Grey1 = Color(0xFFEDF2F5)
+val Grey2 = Color(0xFF505F6A)
+val Grey3 = Color(0x80000000)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF273037)
