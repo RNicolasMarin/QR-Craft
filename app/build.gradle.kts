d69b9eab9446b3ad2e60ac898361768b6870e677
Nicolas Marin <nicolasmarin1312@gmail.com>
2025-09-02 20:30:44 -0300

    Room library added and some libraries updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.devtoolsKsp)
     alias(libs.plugins.kotlin.serialization)
 }
 
@@ -95,4 +96,9 @@ dependencies {
 
     // ZXing to create QR Codes
     implementation(libs.core)
+
+    //Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 }
